ca84faaa126e20ee1e6fa40b9543ce676e857d74
John Victor <johnvictorio3@gmail.com>
2019-02-09 21:59:09 -0300

    WIP: cursosResources

--- src/main/java/com/startworksgroup/socialcourses/resources/CursosResources.java
+++ src/main/java/com/startworksgroup/socialcourses/resources/CursosResources.java
@@ -1,10 +1,14 @@
 package com.startworksgroup.socialcourses.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +31,20 @@ public class CursosResources {
 	@PostMapping
 	public void salvar(@RequestBody Curso curso) {
 		cursosRepository.save(curso);	
+	}
+	
+	@PutMapping("{id}")
+	public void atualizar(@RequestBody Curso curso, @PathVariable("id") Long id) {
+		curso.setId(id);
+		cursosRepository.save(curso);	
+	}
+	@GetMapping("{id}")
+	public Optional<Curso> buscar(@PathVariable("id") Long id) {
+		return cursosRepository.findById(id);
+	}
+	
+	@DeleteMapping("{id}")
+	public void deletar(@PathVariable("id") Long id) {
+		cursosRepository.deleteById(id);
 	}
 }
