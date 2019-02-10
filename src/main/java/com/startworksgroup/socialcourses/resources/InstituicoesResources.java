9121dd45cc069327b956c2d522fe52a906b30cd1
John Victor <johnvictorio3@gmail.com>
2019-02-09 22:02:40 -0300

    WIP: instituicoesResources

--- src/main/java/com/startworksgroup/socialcourses/resources/InstituicoesResources.java
+++ src/main/java/com/startworksgroup/socialcourses/resources/InstituicoesResources.java
@@ -1,10 +1,14 @@
 package com.startworksgroup.socialcourses.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +31,20 @@ public class InstituicoesResources {
 	@PostMapping
 	public void salvar(@RequestBody Instituicao instituicao) {
 		instituicoesRepository.save(instituicao);	
+	}
+	
+	@PutMapping("{id}")
+	public void atualizar(@RequestBody Instituicao instituicao, @PathVariable("id") Long id) {
+		instituicao.setId(id);
+		instituicoesRepository.save(instituicao);	
+	}
+	@GetMapping("{id}")
+	public Optional<Instituicao> buscar(@PathVariable("id") Long id) {
+		return instituicoesRepository.findById(id);
+	}
+	
+	@DeleteMapping("{id}")
+	public void deletar(@PathVariable("id") Long id) {
+		instituicoesRepository.deleteById(id);
 	}
 }
