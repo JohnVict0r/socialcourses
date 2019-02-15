4172e39cb2d25e5cb0a6b856b1852ac5bf8c8728
John Victor <johnvictorio3@gmail.com>
2019-02-15 20:39:19 -0300

    fix(CursosResources): Tratando respostas e adequando os status http corretos

--- src/main/java/com/startworksgroup/socialcourses/resources/CursosResources.java
+++ src/main/java/com/startworksgroup/socialcourses/resources/CursosResources.java
@@ -1,9 +1,13 @@
 package com.startworksgroup.socialcourses.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.startworksgroup.socialcourses.domain.Curso;
 import com.startworksgroup.socialcourses.repository.CursosRepository;
@@ -24,27 +29,49 @@ public class CursosResources {
 	private CursosRepository cursosRepository;
 	
 	@GetMapping
-	public List<Curso> listar() {
-		return cursosRepository.findAll();
+	public ResponseEntity<List<Curso>> listar() {
+		List<Curso> result = cursosRepository.findAll();
+		
+		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 	
 	@PostMapping
-	public void salvar(@RequestBody Curso curso) {
-		cursosRepository.save(curso);	
+	public ResponseEntity<Void> salvar(@RequestBody Curso curso) {
+		curso = cursosRepository.save(curso);
+	
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(curso.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping("{id}")
-	public void atualizar(@RequestBody Curso curso, @PathVariable("id") Long id) {
+	public ResponseEntity<Void> atualizar(@RequestBody Curso curso, @PathVariable("id") Long id) {
 		curso.setId(id);
 		cursosRepository.save(curso);	
+		
+		return ResponseEntity.noContent().build();
 	}
 	@GetMapping("{id}")
-	public Optional<Curso> buscar(@PathVariable("id") Long id) {
-		return cursosRepository.findById(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Optional<Curso> curso = cursosRepository.findById(id);
+		
+		if(!curso.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(curso); 
 	}
 	
 	@DeleteMapping("{id}")
-	public void deletar(@PathVariable("id") Long id) {
-		cursosRepository.deleteById(id);
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+
+		try {
+			cursosRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.noContent().build();
 	}
 }
